046f933e536350e04aeb1d9adc927031369eb664
spin3l <dev.christianlopezgarcia@gmail.com>
2026-03-20 16:20:10 +0100

    chore: updated contracts

--- test/tests/test_metadatarequestmanager.py
+++ test/tests/test_metadatarequestmanager.py
@@ -1,56 +1,95 @@
+import asyncio
 import json
 import logging
 from pathlib import Path
 
 import pytest
+import pytest_asyncio
+from eth_account import Account
+from eth_typing import ChecksumAddress
 from src.config import Settings
 from src.manager import Manager
+from src.ocean import create_asset
 from src.queries import get_metadata_request_from_id
 from src.subgraph import Subgraph
 from web3 import Web3
 
 logger = logging.getLogger(__name__)
 
-zero_address = "0x0000000000000000000000000000000000000000"
+
+@pytest_asyncio.fixture(scope="function")
+async def metadata_request_manager(settings):
+    return await Manager.create(
+        settings.RPC_URL,
+        settings.PRIVATE_KEY.get_secret_value(),
+        settings.OCEAN_ARTIFACTS_FOLDER,
+        "MetadataRequestManager",
+        settings.contract_address("development", "MetadataRequestManager"),
+    )
 
 
-@pytest.fixture(scope="session")
-async def dataset_algorithm_address(settings: Settings):
-    from eth_account import Account
-    from src.ocean import create_asset
-
+@pytest_asyncio.fixture(scope="function")
+async def dataset_algorithm_address(
+    metadata_request_manager: Manager,
+    settings: Settings,
+):
     store_path = Path(".addresses.json")
 
-    if not store_path.exists():
-        account = Account.from_key(settings.PRIVATE_KEY.get_secret_value())
-        dataset_address, algorithm_address = (
-            create_asset(account, asset_type="dataset"),
-            create_asset(account, asset_type="algorithm"),
-        )
-
-        store_path.write_text(
-            json.dumps(
-                {
-                    "dataset_address": str(dataset_address),
-                    "algorithm_address": str(algorithm_address),
-                }
-            )
-        )
-
-    else:
+    if store_path.exists():
         stored = json.loads(store_path.read_text())
         dataset_address, algorithm_address = (
             Web3.to_checksum_address(stored["dataset_address"]),
             Web3.to_checksum_address(stored["algorithm_address"]),
         )
 
-    yield dataset_address, algorithm_address
+        # Check if they exist
+        async def address_exists(address: ChecksumAddress) -> bool:
+            code = await metadata_request_manager.web3.eth.get_code(address)
+            return code not in (b"", b"\x00")
+
+        if await address_exists(dataset_address) and await address_exists(
+            algorithm_address
+        ):
+            return dataset_address, algorithm_address
+
+        logger.info("Current asset addresses not found, creating new")
+
+    account = Account.from_key(settings.PRIVATE_KEY.get_secret_value())
+    dataset_address, algorithm_address = (
+        create_asset(account, asset_type="dataset"),
+        create_asset(account, asset_type="algorithm"),
+    )
+
+    store_path.write_text(
+        json.dumps(
+            {
+                "dataset_address": str(dataset_address),
+                "algorithm_address": str(algorithm_address),
+            }
+        )
+    )
+
+    return dataset_address, algorithm_address
 
 
 @pytest.mark.asyncio
 async def test_nft_created(dataset_algorithm_address):
     logging.info("NFT Address %s", dataset_algorithm_address)
     assert dataset_algorithm_address
+
+
+@pytest.mark.asyncio
+async def test_voting_weight_oracle_set(metadata_request_manager: Manager):
+    response = (
+        await metadata_request_manager.contract.functions.votingWeightOracle().call()
+    )
+
+    checksum_response = Web3.to_checksum_address(response)
+    checksum_zeros = Web3.to_checksum_address(
+        "0x0000000000000000000000000000000000000000"
+    )
+
+    assert checksum_response != checksum_zeros
 
 
 def test_get_asset():
@@ -63,32 +102,31 @@ def test_get_asset():
 
 @pytest.mark.asyncio
 async def test_create_and_verify_subgraph(
-    settings: Settings, dataset_algorithm_address
+    settings: Settings,
+    metadata_request_manager,
+    dataset_algorithm_address,
 ):
     dataset_address, algorithm_address = dataset_algorithm_address
 
-    manager = await Manager.create(
-        settings.RPC_URL,
-        settings.PRIVATE_KEY.get_secret_value(),
-        settings.OCEAN_ARTIFACTS_FOLDER,
-        "MetadataRequestManager",
-        settings.contract_address("development", "MetadataRequestManager"),
-    )
-
     # 2. Trigger the Request
-    receipt = await manager.call(
+    receipt = await metadata_request_manager.call(
         "createRequest",
-        dataset_address,
-        algorithm_address,
-        [1 & 0xFF],
-        ["test"],
-        "I want this!",
+        {
+            "datasetAddress": dataset_address,
+            "algorithmAddress": algorithm_address,
+            "requestTypes": [1 & 0xFF],
+            "data": ["test"],
+            "reason": "I want this!",
+            "expiresIn": 86400,  # 1 day in secs
+        },
     )
 
     assert receipt["status"] == 1, "Blockchain transaction failed"
 
     # 3. Extract ID from logs
-    logs = manager.contract.events.RequestCreated().process_receipt(receipt)
+    logs = metadata_request_manager.contract.events.RequestCreated().process_receipt(
+        receipt
+    )
     logger.info("Logs %s", logs)
 
     request_id = str(logs[0]["args"]["id"])
